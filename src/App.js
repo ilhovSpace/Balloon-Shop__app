698c6a3bd951254dd1b79294538214556bced0e5
ilhov.space__home <ilhov.space@gmail.com>
2020-05-20 15:06:34 +0300

    added base components and router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import AppRouter from "./router/AppRouter";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      App Balloon shop - Develop
-    </div>
+    <AppRouter>
+      <div className="app">
+        App Balloon shop - Develop
+      </div>
+    </AppRouter>
   );
 }
 
